035b258510757a49630dd4cd60d70d44d1f5792a
rocawear <96464612+rocawear-public@users.noreply.github.com>
2023-01-09 19:41:59 +0200

    nodemon for server

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,36 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
-import { Outlet, RouterProvider, Link, createReactRouter, createRouteConfig } from "@tanstack/react-router";
-
-const rootRoute = createRouteConfig({
-  component: () => (
-    <>
-      <div>
-        <Link to="/">Home</Link> <Link to="/about">About</Link>
-      </div>
-      <hr />
-      <Outlet />
-    </>
-  ),
-});
-
-const indexRoute = rootRoute.createRoute({
-  path: "/",
-  component: RoomFurnitures,
-});
-
-const aboutRoute = rootRoute.createRoute({
-  path: "/about",
-  component: About,
-});
-
-function About() {
-  return <div>Hello from About!</div>;
-}
-
-const routeConfig = rootRoute.addChildren([indexRoute, aboutRoute]);
-
-const router = createReactRouter({ routeConfig });
 import { RoomFurnitures } from "./components/RoomFurnitures";
 import { trpc } from "./utils/trpc";
 import { useTrpc } from "./hooks/useTrpc";
@@ -42,15 +11,9 @@ export function App() {
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <StrictMode>
-          <RouterProvider router={router} />
+          <RoomFurnitures />
         </StrictMode>
       </QueryClientProvider>
     </trpc.Provider>
   );
-}
-
-declare module "@tanstack/react-router" {
-  interface RegisterRouter {
-    router: typeof router;
-  }
 }
